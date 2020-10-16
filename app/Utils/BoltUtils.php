76a5738dac2031584ff402613e665729e9dbb3ce
Chonwai Un <chonwaiun@outlook.com>
2020-10-17 02:43:31 +0800

    Delete: Deleted some useless code.

--- app/Utils/BoltUtils.php
+++ app/Utils/BoltUtils.php
@@ -8,9 +8,9 @@ class BoltUtils
 {
     public static function makeConnect()
     {
-        $bolt = new Bolt($ip = env('NEO4J_HOST', 'localhost'), $port = env('NEO4J_PORT', '7687'), $timeout = 120);
+        $bolt = new Bolt($ip = env('NEO4J_HOST'), $port = env('NEO4J_PORT'), $timeout = 120);
         $bolt->setProtocolVersions(4.1);
-        $bolt->init(env('NEO4J_DATABASE', 'Chinese Regional Cuisine Tiny'), env('NEO4J_USERNAME', 'neo4j'), env('NEO4J_PASSWORD', '1234'));
+        $bolt->init(env('NEO4J_DATABASE'), env('NEO4J_USERNAME'), env('NEO4J_PASSWORD'));
         return $bolt;
     }
 }
