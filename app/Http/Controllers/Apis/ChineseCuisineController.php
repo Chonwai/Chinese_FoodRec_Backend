fe96cd9338e8528ee8ee62e8c324f37b70f994c8
Chonwai Un <chonwaiun@outlook.com>
2020-10-16 16:40:02 +0800

    Add: Added the Apis Controller.

---
+++ app/Http/Controllers/Apis/ChineseCuisineController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Apis;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChineseCuisineController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Apis/DishController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Apis;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DishController extends Controller
+{
+    //
+}
