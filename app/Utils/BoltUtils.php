44f955f99d6be4914b1b312c07821c45f94710f1
Chonwai Un <chonwaiun@outlook.com>
2020-10-17 02:23:20 +0800

    Update: Updated the timeout time.

--- app/Utils/BoltUtils.php
+++ app/Utils/BoltUtils.php
@@ -8,7 +8,7 @@ class BoltUtils
 {
     public static function makeConnect()
     {
-        $bolt = new Bolt($ip = env('NEO4J_HOST', 'localhost'), $port = env('NEO4J_PORT', '7687'));
+        $bolt = new Bolt($ip = env('NEO4J_HOST', 'localhost'), $port = env('NEO4J_PORT', '7687'), $timeout = 120);
         $bolt->setProtocolVersions(4.1);
         $bolt->init(env('NEO4J_DATABASE', 'Chinese Regional Cuisine Tiny'), env('NEO4J_USERNAME', 'neo4j'), env('NEO4J_PASSWORD', '1234'));
         return $bolt;
